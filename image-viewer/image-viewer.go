621dd5ffd96c627cb41abf18703e12de7355a71e
pocke <p.ck.t22@gmail.com>
2014-12-14 12:00:14 +0900

    2.3 quit button Implement by go

--- image-viewer/image-viewer.go
+++ image-viewer/image-viewer.go
@@ -11,6 +11,13 @@ func main() {
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
 
 	window.SetSizeRequest(300, 200)
+
+	button := gtk.NewButtonWithLabel("Quit")
+	window.Add(button)
+	button.Connect("clicked", func() {
+		gtk.MainQuit()
+	})
+
 	window.ShowAll()
 	gtk.Main()
 }
