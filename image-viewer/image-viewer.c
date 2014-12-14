6ed74b7758aeec8871b008dd92d09d0ff0f97bf7
pocke <p.ck.t22@gmail.com>
2014-12-15 08:04:24 +0900

    show image

--- image-viewer/image-viewer.c
+++ image-viewer/image-viewer.c
@@ -1,4 +1,5 @@
 #include <gtk-2.0/gtk/gtk.h>
+#include <stdlib.h>
 
 static void  cb_button_clicked(GtkWidget *button, gpointer user_data) {
   gtk_main_quit();
@@ -6,18 +7,41 @@ static void  cb_button_clicked(GtkWidget *button, gpointer user_data) {
 
 int main(int argc, char** argv)
 {
-  GtkWidget *window;
+
+  // check arguments
+  if (argc != 2) {
+    g_print("Usage: %s image-file\n", argv[0]);
+    exit(1);
+  }
 
   gtk_init(&argc, &argv);
 
+  // window
+  GtkWidget *window;
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_set_size_request(window, 300, 200);
 
+
+  // button
   GtkWidget *button;
   button = gtk_button_new_with_label("Quit");
-  gtk_container_add(GTK_CONTAINER(window), button);
 
   g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_button_clicked), NULL);
+
+
+  // image
+  GtkWidget *image;
+  image = gtk_image_new_from_file(argv[1]);
+
+
+  // vbox
+  GtkWidget *vbox;
+  vbox = gtk_vbox_new(FALSE, 2);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+  gtk_box_pack_start(GTK_BOX(vbox), image,  TRUE,  TRUE,  0);
+  gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+
+
 
 
   gtk_widget_show_all(window);
