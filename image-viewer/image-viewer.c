b365c0542d090f9a771bf1849385c09ff0e231a0
pocke <p.ck.t22@gmail.com>
2014-12-14 11:55:22 +0900

    2.3 quit button (implement  by C)

--- image-viewer/image-viewer.c
+++ image-viewer/image-viewer.c
@@ -1,5 +1,9 @@
 #include <gtk-2.0/gtk/gtk.h>
 
+static void  cb_button_clicked(GtkWidget *button, gpointer user_data) {
+  gtk_main_quit();
+}
+
 int main(int argc, char** argv)
 {
   GtkWidget *window;
@@ -9,8 +13,16 @@ int main(int argc, char** argv)
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_set_size_request(window, 300, 200);
 
-  gtk_widget_show(window);
+  GtkWidget *button;
+  button = gtk_button_new_with_label("Quit");
+  gtk_container_add(GTK_CONTAINER(window), button);
+
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_button_clicked), NULL);
+
+
+  gtk_widget_show_all(window);
   gtk_main();
 
   return 0;
 }
+
