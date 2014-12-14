f2e97e0db03d863353ab450e0f7981bfbe161dc5
pocke <p.ck.t22@gmail.com>
2014-12-15 08:06:30 +0900

    refactor

--- image-viewer/image-viewer.c
+++ image-viewer/image-viewer.c
@@ -20,28 +20,30 @@ int main(int argc, char** argv)
   GtkWidget *window;
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_set_size_request(window, 300, 200);
+  {
+    // vbox
+    GtkWidget *vbox;
+    vbox = gtk_vbox_new(FALSE, 2);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    {
+      // image
+      GtkWidget *image;
+      image = gtk_image_new_from_file(argv[1]);
 
 
-  // button
-  GtkWidget *button;
-  button = gtk_button_new_with_label("Quit");
+      gtk_box_pack_start(GTK_BOX(vbox), image,  TRUE,  TRUE,  0);
+    }
 
-  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_button_clicked), NULL);
+    {
+      // button
+      GtkWidget *button;
+      button = gtk_button_new_with_label("Quit");
 
-
-  // image
-  GtkWidget *image;
-  image = gtk_image_new_from_file(argv[1]);
-
-
-  // vbox
-  GtkWidget *vbox;
-  vbox = gtk_vbox_new(FALSE, 2);
-  gtk_container_add(GTK_CONTAINER(window), vbox);
-  gtk_box_pack_start(GTK_BOX(vbox), image,  TRUE,  TRUE,  0);
-  gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
-
-
+      g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_button_clicked), NULL);
+      gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+    }
+  }
 
 
   gtk_widget_show_all(window);
