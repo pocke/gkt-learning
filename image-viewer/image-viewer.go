495b1621d14e804c183f427e4aa69d54fd4980f8
pocke <p.ck.t22@gmail.com>
2014-12-15 09:39:04 +0900

    image implement Golang

--- image-viewer/image-viewer.go
+++ image-viewer/image-viewer.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/mattn/go-gtk/gtk"
 )
 
 func main() {
-	gtk.Init(&os.Args)
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s image-file\n", os.Args[0])
+		os.Exit(1)
+	}
 
+	gtk.Init(&os.Args)
+
+	// window
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
 	window.SetSizeRequest(300, 200)
 
+	// vbox
+	vbox := gtk.NewVBox(false, 2)
+	window.Add(vbox)
+
+	// image
+	image := gtk.NewImageFromFile(os.Args[1])
+	vbox.PackStart(image, true, true, 0)
+
+	// button
 	button := gtk.NewButtonWithLabel("Quit")
-	window.Add(button)
 	button.Connect("clicked", func() {
 		gtk.MainQuit()
 	})
+	vbox.PackStart(button, false, false, 0)
 
 	window.ShowAll()
 	gtk.Main()
